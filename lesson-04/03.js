8042466148cdf28d25a0085b314c0c16ba7b64bf
Zahar <feelnoregret@gmail.com>
2025-10-07 14:35:49 +0300

    task 3 from block 4 completed

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -17,4 +17,21 @@ findCommonElements([1, 2, 3], [2, 3, 4]) // [2, 3]
 Подсказка: можно использовать функцию `includesElement`, которую мы написали ранее. Переписывать её не нужно, она доступна по всему проекту за счёт hoisting.
 */
 
-function findCommonElements() {}
+function findCommonElements(someArray, otherArray) {
+  let newArray = [];
+  for (let i = 0; i < someArray.length; i++) {
+    const element1 = someArray[i];
+
+    for (let i = 0; i < otherArray.length; i++) {
+      const element2 = otherArray[i];
+      if (element1 === element2) {
+        newArray.push(element1);
+      } else {
+        continue;
+      }
+    }
+  }
+  return newArray;
+}
+let solution = findCommonElements(array1, array2);
+console.log(solution);
