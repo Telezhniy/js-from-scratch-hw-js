50e07eb460792be6a4ac01c7d4a501c297aaef68
Zahar <feelnoregret@gmail.com>
2025-10-12 15:04:12 +0300

    task 1 from block 5 is done

--- lesson-05/01.js
+++ lesson-05/01.js
@@ -15,5 +15,15 @@ const game = {
     gold: 250,
     lumber: 100,
   },
-  addResource() {}
-}
+  addResource(resource, amount) {
+    if (resource === "gold") {
+      game.resources.gold = game.resources.gold + amount;
+      return game.resources.gold;
+    } else if (resource === "lumber") {
+      game.resources.lumber = game.resources.lumber + amount;
+      return game.resources.lumber;
+    } else {
+      console.log("Invalid resource");
+    }
+  },
+};
